91426cec4b919af066d17c4159132ee70281d852
judeal3000 <99591546+judeal3000@users.noreply.github.com>
2022-06-07 18:10:19 -0500

    MACX

--- block.py
+++ block.py
@@ -1,16 +1,47 @@
-from time import time
+import time
+import util.crypto_hash as sha
+import util.db as db
+
 
 def compile_tx(txs):
-    
+    a = ""
+    for i in txs:
+        a += str(i)
+    return sha.crypto_hash(a)
+
 
 class Block:
-    def __init__(self, prev, version, txs, win):
-        //key
+    def __init__(self, prev, version, txs):
+        # key
+        self.hash = ""
         self.previous = prev
+        self.win_attempt = ""
         self.version = version
-        self.win_attempt = win
         self.merkle_root = compile_tx(txs)
 
-        //value
+        # value
         self.time = int(time.time())
         self.transacions = txs
+
+    def getPlay(self):
+        return sha.crypto_hash(str(self.previous)+str(self.version)+str(self.merkle_root)+str(self.time))
+
+    def addWin(self, win):
+        self.win_attempt = win
+        self.hash = sha.crypto_hash(
+            str(self.previous)+str(self.win_attempt)+str(self.version)+str(self.merkle_root)+str(self.time))
+
+    def toDict(self):
+        dict = {
+            "hash": self.hash,
+            "prev": self.previous,
+            "win": self.win_attempt,
+            "version": self.version,
+            "merkle": self.merkle_root,
+            "time": self.time,
+            "txs": self.transacions
+        }
+        return dict
+
+    def storeBlock(self):
+        db.dbPut(self.hash, self.toDict())
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from hashlib import new
 from util.crypto_hash import crypto_hash
 from util.hex_to_binary import hex_to_binary
+import block
+import util.db as db
 
-print(hex_to_binary("b49177e05868b7af8e82a644c1ce20e521af46497adeaffe861d294d9b4bb75e"))
-print(crypto_hash("hi"))
+block = block.Block(
+    '6B51D431DF5D7F141CBECECCF79EDF3DD861C3B4069F0B11661A3EEFACBBA918', 2.0, [3, 4])
+block.addWin('6B51D431DF5D7F141CBECECCF79EDF3DD861C3B4069F0B11661A3EEFACBBA918')
+block.storeBlock()
+print(db.dbGet(block.hash))
